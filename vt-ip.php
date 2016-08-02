8f6565b9da042a11b5fac27451418707d8d1862a
Herb Miller <herb@bobbingwide.com>
2016-08-02 17:34:04 +0100

    Find IPs to block by counting the number of login requests per IP

--- vt-ip.php
+++ vt-ip.php
@@ -4,22 +4,31 @@
  * Syntax: php vt-ip.php $file
  * 
  * @TODO Make it work off parameters rather than hardcoded.
+ 
  *
+ * Should also be able to look for specific requests
+ * 
+ * Request |	Action to perform if seen too often
+ * -------- | -----------------------------------
+ * wp-login.php | block the IP address
  */
 
 function vt_ip_loaded() {
 	$year = "2016";
+	$host = "oik-plugins.com"; 
 	$host = "bobbingwide.org.uk";
-	$mmdd = "0728";
+	$mmdd = "0731";
+	$mmdd = "0802";
 	$file = "vt$year/$host/$mmdd.vt";
+	$match_request = "wp-login.php"; 
 	$contents = file( $file );
 	global $ips;
 	$ips = array();
 	//echo count( $contents) ;
-	count_ips( $contents );
+	count_ips( $contents, $match_request );
 	//print_r( $ips );
 	
-	print_ips( 100, $file, $contents );
+	print_ips( 80, $file, $contents, $match_request );
 	
 }
 
@@ -57,15 +66,16 @@ vt_ip_loaded();
 		`
  *		
  */
-function count_ips( $contents ) {
+function count_ips( $contents, $match_request='wp-login.php' ) {
 	foreach ( $contents as $content ) {
 		$data = str_getcsv( $content );
 		//print_r( $data );
 		//gob();
+		$request = $data[0];
 		$ip = $data[16] ;
 		$elapsed = $data[2];
 		$http_user_agent = $data[19];
-		add_ip( $ip, $elapsed, $http_user_agent );
+		add_ip( $ip, $elapsed, $http_user_agent, $request, $match_request );
 	}
 }
 
@@ -76,16 +86,20 @@ function count_ips( $contents ) {
  * @param float $elapsed - the elapsed time for the request
  * @param string $http_user_agent 
  */
-function add_ip( $ip, $elapsed, $http_user_agent ) { 
+function add_ip( $ip, $elapsed, $http_user_agent, $request, $match_request="wp-login.php" ) { 
 	global $ips;
 	
 	if ( !isset( $ips[ $ip ] ) ) {
 		$http_user_agent = str_replace( ",", " ", $http_user_agent );
-		$ips[ $ip ] = array( 0, 0.0, $http_user_agent );
+		$ips[ $ip ] = array( 0, 0.0, 0, 0.0, $http_user_agent );
 	}
 	$ips[ $ip ][0] = $ips[ $ip ][0] + 1;
 	$ips[ $ip ][1] = $ips[ $ip ][1] + $elapsed;
-	// $ips[ $ip ][2] = 
+	if ( false !== strpos( $request, $match_request ) ) {
+	  $ips[ $ip ][2] = $ips[ $ip ][2] + 1; 
+		$ips[ $ip ][3] = $ips[ $ip ][3] + $elapsed;
+	}
+ 
 }
 
 /**
@@ -97,33 +111,43 @@ function add_ip( $ip, $elapsed, $http_user_agent ) {
  * @param string $file name - so we know where the information comes from
  * @param array $content - the file contents 
  */
-function print_ips( $limit, $file, $contents ) {
+function print_ips( $limit, $file, $contents, $match_request ) {
 	global $ips;
 	$rest_ip_count = 0;
 	$rest_count = 0;
 	$rest_elapsed = 0;
+	$rest_match_count = 0;
+	$rest_match_elapsed = 0;
 	$total_ips = 0;
 	$total_elapsed = 0;
+	$total_match_count = 0;
+	$total_match_elapsed = 0;
 	
-	echo "IP,count,elapsed,HTTP_user_agent $file" . PHP_EOL; 
+	echo "IP,count,elapsed,match count,match elapsed,HTTP_user_agent $file" . PHP_EOL; 
   foreach ( $ips as $ip => $data ) {
 		$count = $data[0];
 		$elapsed = $data[1];
-		$http_user_agent = $data[2];
+		$match_count = $data[2];
+		$match_elapsed = $data[3];
+		$http_user_agent = $data[4];
 		if ( $count >= $limit ) {
 
-			echo "$ip,$count,$elapsed,$http_user_agent" . PHP_EOL;
+			echo "$ip,$count,$elapsed,$match_count,$match_elapsed,$http_user_agent" . PHP_EOL;
 		} else {
 			$rest_ip_count++;
 			$rest_count += $count;
 			$rest_elapsed += $elapsed;
+			$rest_match_count += $match_count;
+			$rest_match_elapsed += $match_elapsed;
 		}
 		$total_ips++;
 		$total_elapsed += $elapsed;
+		$total_match_count += $match_count;
+		$total_match_elapsed += $match_elapsed;
 	}
 	$total_requests = count( $contents );
-	echo "the.rest.lt.$limit,$rest_count,$rest_elapsed,$rest_ip_count" . PHP_EOL;
-	echo "TOTAL,$total_requests,$total_elapsed,$total_ips" . PHP_EOL;
+	echo "the.rest.lt.$limit,$rest_count,$rest_elapsed,$rest_match_count,$rest_match_elapsed,$rest_ip_count" . PHP_EOL;
+	echo "TOTAL,$total_requests,$total_elapsed,$total_match_count,$total_match_elapsed,$total_ips" . PHP_EOL;
 }	
 		
 
