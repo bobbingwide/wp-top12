c29761e2cd697c1def4c8a268cbdcf1dc2dfb51f
Herb Miller <herb@bobbingwide.com>
2021-01-23 19:08:41 +0000

    Issue #8 - add build files

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dd1e85a8f252020d27b5ca8339428e24');
